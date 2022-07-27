c2b8b92c2a3e2325d3222164f902bb9b7de9e4ff
sukhada121 <Sukhada.Virkar@data-axle.com>
2022-07-27 22:37:42 +0530

    dot net web app

---
+++ Dot Net assignment/DotNetCsharp/Migrations/20220727165431_seeddata.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCsharp.Migrations
+{
+    public partial class seeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Registration",
+                columns: new[] { "Id", "Email", "Password", "Username" },
+                values: new object[] { 1, "john21@gmail.com", "john@123", "John" });
+
+            migrationBuilder.InsertData(
+                table: "Registration",
+                columns: new[] { "Id", "Email", "Password", "Username" },
+                values: new object[] { 2, "christk@gmail.com", "chris@220", "Chris" });
+
+            migrationBuilder.InsertData(
+                table: "Registration",
+                columns: new[] { "Id", "Email", "Password", "Username" },
+                values: new object[] { 3, "mukesh@gmail.com", "muk@123", "Mukesh" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Registration",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Registration",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Registration",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
---
+++ Dot Net assignment/DotNetCsharp/Models/RegistrationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DotNetCsharp.Models
+{
+    public class RegistrationContext: DbContext
+    {
+        public RegistrationContext(DbContextOptions<RegistrationContext> options): base(options)
+        {
+
+        }
+        public DbSet<Registration> Registration { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Registration>().HasData(
+                new Registration() { Id = 1, Username = "John", Email="john21@gmail.com", Password="john@123" },
+                new Registration() { Id = 2, Username = "Chris", Email = "christk@gmail.com", Password = "chris@220" },
+                new Registration() { Id = 3, Username = "Mukesh", Email = "mukesh@gmail.com", Password = "muk@123" });
+        }
+    }
+}
